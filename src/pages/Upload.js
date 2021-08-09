827ec498c729296103cc77f6463bed6f51d2d416
betalantz <betalantz@gmail.com>
2021-08-09 11:58:33 -0700

    Revert "reformats file str for Cloudinary upload"
    This reverts commit 0aa5084df3d4ecb9a7a938a0aa4500034d8248ff.

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -11,11 +11,10 @@ export default function Upload() {
 
     const handleUploadClick = async () => {
         setUploadStatus('Uploading . . .')
-        const filename = file.split('.')[0] + '.jpg'
-        console.log('filename: ', filename);
+
         const imageData = new FormData()
         imageData.append('upload_preset', 'stand-app')
-        imageData.append('file', filename)
+        imageData.append('file', file)
 
         // 1. store the image on eg. Cloudinary, get the link 
         console.log('Storing image')
